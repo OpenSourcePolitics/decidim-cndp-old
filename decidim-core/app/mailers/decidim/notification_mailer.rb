ef94189ff1094929b1d37a44a38d151bb32b87bf
Ludivine Coipel <coipel.ludivine@gmail.com>
2017-12-20 16:46:35 +0100

    Set mailer for new content

--- decidim-core/app/mailers/decidim/notification_mailer.rb
+++ decidim-core/app/mailers/decidim/notification_mailer.rb
@@ -8,15 +8,25 @@ module Decidim
     helper Decidim::TranslationsHelper
 
     def event_received(event, event_class_name, resource, user, extra)
-      moderation = extra[:moderation_event]
-      template = moderation ? "event_to_moderate_received" : "event_received"
+      with_user(user) do
+        @organization = resource.organization
+        event_class = event_class_name.constantize
+        @event_instance = event_class.new(resource: resource, event_name: event, user: user, extra: extra)
+        subject = @event_instance.email_subject
+
+        mail(to: user.email, subject: subject)
+      end
+    end
+
+    def new_content_received(event, event_class_name, resource, user, extra)
+      @moderation = extra[:moderation_event]
       with_user(user) do
         @organization = resource.organization
         event_class = event_class_name.constantize
         @event_instance = event_class.new(resource: resource, event_name: event, user: user, extra: extra)
         @slug = extra[:process_slug]
         @locale = locale.to_s
-        subject = moderation ? @event_instance.email_moderation_subject : @event_instance.email_subject
+        subject = @event_instance.email_moderation_subject
         @parent_title = if is_proposal?(resource)
                           resource.feature.participatory_space.title
                         else
@@ -25,15 +35,19 @@ module Decidim
 
         @resource_title = resource.title if is_proposal?(resource)
         @body = if is_proposal?(resource)
-                   resource.body
+                  resource.body
                 else
                   Decidim::Comments::Comment.find(extra[:comment_id]).body
                 end
+
         @moderation_url = "http://" + @organization.host + "/admin/participatory_processes/" + @slug + "/moderations?locale=" + @locale + "&moderation_type=upstream"
         @is_proposal = is_proposal?(resource)
-        mail(to: user.email, subject: subject, :template_name => template)
+
+        mail(to: user.email, subject: subject)
       end
     end
+
+    private
 
     def is_proposal?(resource)
       resource.is_a?(Decidim::Proposals::ProposalCreatedEvent) || resource.is_a?(Decidim::Proposals::Proposal )
